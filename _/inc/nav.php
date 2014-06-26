0f8c79f5841916ee15372c2366d5163272e37ac6
Ryan Maskell <ryan@savorweb.com>
2014-06-25 17:17:13 -0700

    things

--- _/inc/nav.php
+++ _/inc/nav.php
@@ -5,8 +5,9 @@
 		<button type="button" role="button" id="nav-toggle" class="lines-button x2"><span class="lines"></span></button>
 
 		<nav class="nav-collapse">
+			<small>Call to schedule a consultation <a href="tel:7863530856">786.353.0856</a></small>
 			<ul>
-				<li><a href="/contact">Contact</a></li>
+				<li><a href="/contact">Commercial</a></li>
 				<li><a href="/product">Custom Homes</a></li>
 				<li><a href="/about">Condominiums</a></li>
 			</ul>
